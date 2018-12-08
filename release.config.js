4d6ad2aa2491f600bde819c75bd0795d5bd19cda
Alex Kluew <dropbyalex@gmail.com>
2018-12-08 10:31:27 -0500

    feat(auto): add semantic-release/changelog for auto change long generation

--- release.config.js
+++ release.config.js
@@ -4,6 +4,7 @@ exports = module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
     '@semantic-release/github'
   ]
 };
