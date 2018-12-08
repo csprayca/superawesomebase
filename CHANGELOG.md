8ecb8dfde73229fef99aedf19f24dbf060fa8aa7
Alex Kluew <getaclue@users.noreply.github.com>
2018-12-08 11:56:20 -0500

    feat(auto): add semantic release git to ci
    * chore(release): v1.0.0 [ci skip] ***NO_CI***
    
    * feat(auto): add @semantic-release/git to try with git automation

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+<a name="1.0.0"></a>
+# 1.0.0 (2018-12-08)
+
+
+### Features
+
+* init project ([4da3b70](https://github.com/csprayca/superawesomebase/commit/4da3b70))
+* **auto:** add semantic-release/changelog for auto change long generation ([4d6ad2a](https://github.com/csprayca/superawesomebase/commit/4d6ad2a))
+
+
+
--- release.config.js
+++ release.config.js
@@ -5,6 +5,15 @@ exports = module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    '@semantic-release/github'
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+        message: `chore(release): ${nextRelease.version} [skip ci]\n\n${
+          nextRelease.notes
+        }`
+      }
+    ]
   ]
 };
